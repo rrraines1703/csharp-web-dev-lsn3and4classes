11103840eac234ba4e41ab967a537e32c1c70c92
Richard Raines <rrraines1703@me.com>
2020-06-05 10:11:59 -0400

    Completed excercise.

---
+++ SchoolPractice/Course.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolPractice
+{
+    class Course
+    {
+        public string Credits { get; set; }
+        public string Subject { get; set; }
+        public List<Student> students { get; set; }
+    }
+}
